54b4de2029ed6efe07e03c52a8039b2703a626d0
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-30 12:48:51 -0700

    implemented /api/comments/:id put route

--- controllers/api/comments-routes.js
+++ controllers/api/comments-routes.js
@@ -6,7 +6,7 @@ router.post('/', async (req, res) => {
     try {
         const comment = await Comments.create(req.body);
         console.log(comment);
-        res.status(200).json(comment);
+        res.status(200).json({comment: comment, result: "Comment added successfully."});
     } catch (err) {
         res.status(500).send(err);
     }
@@ -14,8 +14,25 @@ router.post('/', async (req, res) => {
 
 // Edit a comment
 router.put('/:id', async (req, res) => {
-    // TODO
-    res.status(200).send(req.params.id);
+    try {
+        const comment = await Comments.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        if(!comment) {
+            res.status(404).json({result: 'Comment not found.'});
+            return;
+        }
+        const updatedComment = await Comments.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({comment: updatedComment, result: 'Comment updated successfully.'})
+    } catch (err) {
+        res.status(500).send(err);
+    }
 });
 
 // Delete a comment
