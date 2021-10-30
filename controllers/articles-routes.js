ef63393041e156adf6472efac87f9df4de1650e5
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-30 15:14:40 -0700

    added /articles get route

--- controllers/articles-routes.js
+++ controllers/articles-routes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
+const {Articles} = require('../models');
 
 // Get a list of all articles
 router.get('/', async (req, res) => {
-    // TODO
-    res.status(200).send('Getting all articles');
+    try {
+        const articles = await Articles.findAll();
+        const articlesJSON = articles.map( (article) => article.get({plain: true}) );
+        res.render('articles', {articles: articlesJSON});
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 // Get's a specific article
