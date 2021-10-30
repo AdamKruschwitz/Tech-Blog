f1e23727401deb384aabeb1586f4beb41b3f7413
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-30 14:05:16 -0700

    Implemented /api/articles/ post route

--- controllers/api/articles-routes.js
+++ controllers/api/articles-routes.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
+const {Articles} = require('../../models');
 
 // Post an article to create a new article
 router.post('/', async (req, res) => {
-    // TODO
-    res.status(200).send(req.body);
+    try {
+        // Exit if not logged in
+        if(!req.session.loggedIn) {
+            res.status(401).json({ response: "Must be logged in to do this action" });
+            return;
+        }
+        // Create a new article
+        const article = Articles.create({
+            title: req.body.title,
+            body: req.body.body,
+            author_id: req.session.curUserID
+        });
+        res.status(200).json({ article: article, response: "Article added successfully."})
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // Update the body of an article
