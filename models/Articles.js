b41714a31c2c77d9ee17255ec3e1415b52cf0d9b
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-29 20:18:33 -0700

    added Articles model

---
+++ models/Articles.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const User = require('./Users');
+const sequelize = require('../config/connection');
+
+class Articles extends Model {}
+
+Articles.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        author_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User
+            }
+        }
+    },
+    {
+        sequelize,
+        modelName: 'articles'
+    }
+)
