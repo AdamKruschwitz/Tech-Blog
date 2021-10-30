8ac3513353dc50c7024954821e58acbeb1cb128c
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-30 15:29:05 -0700

    added /users/ get route

--- controllers/users-routes.js
+++ controllers/users-routes.js
@@ -1,10 +1,18 @@
-const { response } = require('express');
 const router = require('express').Router();
+const { Users, Articles } = require('../models');
 
 // Renders the all users page
 router.get('/', async (req, res) => {
-    // TODO
-    res.status(200).send('getting all users');
+    try {
+        const users = await Users.findAll({
+            include: [{model: Articles}]
+        });
+        const usersJSON = users.map( (user) => user.get({plain: true}) )
+        // console.log(usersJSON);
+        res.render('authors', {authors: usersJSON});
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // Renders a single user page
